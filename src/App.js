0aa359a434ee4bf52e4030c20ad3073715ee664c
Oleh Lutsenko <oleh.lutsenko@silobreaker.com>
2020-10-07 21:57:40 +0200

    add logo and some initial styling

--- src/App.js
+++ src/App.js
@@ -1,7 +1,12 @@
 import React from "react";
+import { ReactComponent as BigLogo } from "./svgs/big_logo.svg";
 
 function App() {
-  return <div className="text-4xl text-white bg-primary-500">Hello!</div>;
+  return (
+    <div className="text-4xl text-white w-full h-full">
+      <BigLogo />
+    </div>
+  );
 }
 
 export default App;
